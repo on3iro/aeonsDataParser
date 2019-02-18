26114bd98caff6721caef86a871c239c994d7b0e
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-02-18 20:21:40 +0100

    Add cardsMap

--- parser.exs
+++ parser.exs
@@ -1,6 +1,19 @@
-nemesesMap = File.stream!("nemesis.js")
-|> Enum.reduce(%{}, fn(line, acc) ->
-  nemesisEntry = Regex.named_captures(~r/["'](?<name>\w+)["'],\s?["'](?<set>\w+)["']/, line)
-  Map.put(acc, nemesisEntry["name"], nemesisEntry)
-end)
+nemesesMap =
+  File.stream!("nemesis.js")
+  |> Enum.reduce(%{}, fn line, acc ->
+    nemesisEntry = Regex.named_captures(~r/["'](?<name>\w+)["'],\s?["'](?<set>\w+)["']/, line)
+    Map.put(acc, nemesisEntry["name"], nemesisEntry)
+  end)
+
+cardsMap =
+  File.stream!("cards.js")
+  |> Enum.reduce(%{}, fn line, acc ->
+    cardEntry =
+      Regex.named_captures(
+        ~r/["'](?<name>\w+)["'],\s?["'](?<type>\w+)["'],\s?(?<cost>\d),\s?["'](?<set>\w+)["']/,
+        line
+      )
+
+    Map.put(acc, cardEntry["name"], cardEntry)
+  end)
 
